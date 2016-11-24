7c5a0ea950aa32007743675472826c9543a1f027
Doug DeBold <ddebold56@gmail.com>
2016-11-23 18:24:36 -0700

    Add polyfills

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,7 +10,14 @@ module.exports = function (defaults) {
       ]
     }
   });
-
+  app.import('bower_components/es5-shim/es5-shim.js', {
+    type: 'vendor',
+    prepend: true
+  });
+  app.import('vendor/object_values_polyfill.js', {
+    type: 'vendor',
+    prepend: true
+  });
   app.import('bower_components/moment/moment.js');
   return app.toTree();
 };
